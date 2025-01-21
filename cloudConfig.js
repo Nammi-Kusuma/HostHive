493bc936a1e0c61b969f64c4423894eae3d60bc8
Nammi-Kusuma <nkusuma892004@gmail.com>
2025-01-21 21:28:12 +0530

    new commit

--- cloudConfig.js
+++ cloudConfig.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-      folder: 'Wanderlust-DEV',
+      folder: 'HostHive-DEV',
       allowerdFormats: ["png", "jpg", "jpeg"],
     },
   });
